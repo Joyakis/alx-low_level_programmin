0986c555389c17bdb21158424129868c87a77864
Joyakis <joyakinyi940@gmail.com>
2022-03-11 08:18:57 -0800

    positivity

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,28 +9,12 @@
  */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n  =  rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n  ==  0)
-		printf("%d is zero\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	return (0);
-}
-
-=======
-printf("Enter n.\n")
-scan f("%d , &n);
-if(n < 0)
+int n;
+printf("Enter n\n")
+scanf("%d, $n);
+if (n < 0.00)
 printf("n is negative\n")
-else if (n > 0)
-printf("n is positive\n")
-else
-printf("n is zero\n");
-return (0);
+else if (n > 0.00)
+printf("n is positive\n");                                                      elseprintf("n is zero\n");	
+return(0);
 }
->>>>>>> 8adb5ab0ff9c4b6b6fbdc4a96d91b5886dc6d251
